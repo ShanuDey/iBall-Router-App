c6cbf57f8e3249c323f462406853815ab6605a92
ShanuDey <sd4shanudey@gmail.com>
2020-01-25 23:14:41 +0530

    Add Traffic Navigation Fragment
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

---
+++ app/src/main/java/com/example/iballbaton/ui/traffic/TrafficFragment.java
@@ -0,0 +1,26 @@
+package com.example.iballbaton.ui.traffic;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.iballbaton.R;
+
+
+public class TrafficFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_traffic,container,false);
+
+
+        return root;
+    }
+
+}
