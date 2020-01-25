5d47159768eb95f4bf9243107df7c60fe5515e16
ShanuDey <sd4shanudey@gmail.com>
2020-01-25 22:46:22 +0530

    Add Logout option in menu bar
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- app/src/main/java/com/example/iballbaton/Dashboard.java
+++ app/src/main/java/com/example/iballbaton/Dashboard.java
@@ -1,11 +1,13 @@
 package com.example.iballbaton;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -65,5 +67,29 @@ public class Dashboard extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if(id == R.id.action_logout){
+            // Remove cookie
+            SharedPreferences sharedPreferences = getSharedPreferences(StaticData.SHRD_PREF_COOKIE,MODE_PRIVATE);
+            sharedPreferences.edit().remove(StaticData.COOKIE_NAME).commit();
+
+            // Loading Login Activity
+            startActivity(new Intent(this,MainActivity.class));
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
