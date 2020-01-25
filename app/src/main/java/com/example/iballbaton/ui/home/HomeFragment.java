97bae545e0b25412a870dd988bfa16eaa7364d47
ShanuDey <sd4shanudey@gmail.com>
2020-01-25 20:53:48 +0530

    Fetch data in background thread pass the data to main/UI thread show the data to respected views convert time stamp modify according to the router api
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- app/src/main/java/com/example/iballbaton/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/iballbaton/ui/home/HomeFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,6 +17,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.alespero.expandablecardview.ExpandableCardView;
 import com.example.iballbaton.R;
 import com.example.iballbaton.StaticData;
 
@@ -36,13 +38,6 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
         HashMap<String, String> system_status = null;
         try {
@@ -53,7 +48,85 @@ public class HomeFragment extends Fragment {
             e.printStackTrace();
         }
         Log.v("shanu: Home: result=", system_status.get("wanIP"));
+
+
+        ExpandableCardView card_wan_status =  root.findViewById(R.id.wan_status);
+
+        // some static value
+        String[] connectionType = new String[]{"Static IP","Dynamic IP","PPPoE", "PPTP", "L2TP"};
+        String[] connectionState = new String[]{"Disconnected","Connecting","Connected"};
+
+        // Fill wan status card_wan_status view's text data
+        ((TextView) card_wan_status.findViewById(R.id.tv_wanIP)).setText(
+                connectionState[
+                        Integer.parseInt(
+                                cleanUp(
+                                    system_status.get("cableDSL")
+                                )
+                        )
+                ]
+        );
+        ((TextView) card_wan_status.findViewById(R.id.tv_wanIP)).setText(cleanUp(system_status.get("wanIP")));
+        ((TextView) card_wan_status.findViewById(R.id.tv_subnetMask)).setText(cleanUp(system_status.get("subMask")));
+        ((TextView) card_wan_status.findViewById(R.id.tv_gateway)).setText(cleanUp(system_status.get("gateWay")));
+        ((TextView) card_wan_status.findViewById(R.id.tv_primaryDNS)).setText(cleanUp(system_status.get("dns1")));
+        ((TextView) card_wan_status.findViewById(R.id.tv_secondaryDNS)).setText(cleanUp(system_status.get("dns2")));
+        ((TextView) card_wan_status.findViewById(R.id.tv_connectionType)).setText(
+                connectionType[
+                        Integer.parseInt(
+                                cleanUp(
+                                        system_status.get("conTypeIdx")
+                                )
+                        )-1 // array starts from 0
+                ]
+        );
+
+        ExpandableCardView card_system_status = root.findViewById(R.id.system_status);
+
+        ((TextView) card_system_status.findViewById(R.id.tv_lan_mac_addr)).setText(cleanUp(system_status.get("lan_mac")));
+        ((TextView) card_system_status.findViewById(R.id.tv_wan_mac_addr)).setText(cleanUp(system_status.get("wan_mac")));
+        ((TextView) card_system_status.findViewById(R.id.tv_system_time)).setText(cleanUp(system_status.get("systime")));
+        ((TextView) card_system_status.findViewById(R.id.tv_running_time)).setText(timeStr(cleanUp(system_status.get("uptime"))));
+        ((TextView) card_system_status.findViewById(R.id.tv_connected_client)).setText(cleanUp(system_status.get("clients")));
+        ((TextView) card_system_status.findViewById(R.id.tv_software_version)).setText(cleanUp(system_status.get("run_code_ver")));
+        ((TextView) card_system_status.findViewById(R.id.tv_hardware_version)).setText(cleanUp(system_status.get("hw_ver")));
+
         return root;
+    }
+
+    private String cleanUp(String s){
+        int lenght = s.length();
+        return s.substring(1,lenght-2);
+    }
+
+    private String timeStr(String time) {
+        long s,m,h,d;
+        long t = Long.parseLong(time);
+        String str = new String();
+        if(t < 0)
+        {
+            str="00:00:00";
+            return str;
+        }
+        s=t%60;
+        m=(t/60)%60;
+        h=(t/3600)%24;
+        d=(t/86400);
+        if (d > 999) {
+            return "Permanent";
+        }
+        if (d!=0) {
+            str+=d+"Day";
+        }
+        str+=fit2(h)+':';
+        str+=fit2(m)+':';
+        str+=fit2(s);
+        return str;
+    }
+
+    private String fit2(Long n){
+        String s=(""+n+100).substring(0,2);
+        return s;
     }
 
     private class LoginAsyncTask extends AsyncTask<String, String, HashMap<String, String>> {
@@ -73,7 +146,7 @@ public class HomeFragment extends Fragment {
 //                Log.v("shanu",document.select("script[language=JavaScript][type$=text/javascript]").outerHtml());
                 String[] data = document.select("script[language=JavaScript][type$=text/javascript]").outerHtml().split("\n");
                 HashMap<String, String> system_status = new HashMap<>();
-                for (int i = 6; i <= 22; i++) {
+                for (int i = 5; i <= 22; i++) {
                     String[] pair = data[i].split("=");
                     system_status.put(pair[0].trim(), pair[1].trim());
                     Log.v("shanu", "Key=" + pair[0] + " Value=" + pair[1]);
