b4b9d16a03d059ea0c8ea9e9ec68d14dc550d1e7
ShanuDey <sd4shanudey@gmail.com>
2020-01-26 11:37:01 +0530

    Extracted trafic statistics API from router by reverse engineering
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- app/src/main/java/com/example/iballbaton/StaticData.java
+++ app/src/main/java/com/example/iballbaton/StaticData.java
@@ -4,6 +4,8 @@ public class StaticData {
     public static final String URL_HOST = "http://192.168.1.1/";
     public static final String URL_LOGIN = URL_HOST + "login.asp";
     public static final String URL_SYSTEM_STATUS = URL_HOST+ "system_status.asp";
+    public static final String URL_TRAFFIC_STATISTICS  = URL_HOST+ "goform/updateIptAccount";
+
 
     public static final String COOKIE_NAME = "ecos_pw";
     public static final String COOKIE_VALUE_SUFFIX = "cvb:language=en";
