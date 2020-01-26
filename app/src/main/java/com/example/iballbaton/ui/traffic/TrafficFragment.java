3d4be22cd449f4579f9812fb8121acaa9f6cb65b
ShanuDey <sd4shanudey@gmail.com>
2020-01-26 21:54:34 +0530

    Traffic: Fetch data bind with recycler view
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- app/src/main/java/com/example/iballbaton/ui/traffic/TrafficFragment.java
+++ app/src/main/java/com/example/iballbaton/ui/traffic/TrafficFragment.java
@@ -9,16 +9,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.iballbaton.R;
 import com.example.iballbaton.StaticData;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -28,25 +34,37 @@ import okhttp3.Response;
 
 
 public class TrafficFragment extends Fragment {
-
+    private RecyclerView recyclerView;
+    private Context context;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_traffic,container,false);
-
-        new BackgroudAsyncTask().execute(StaticData.URL_TRAFFIC_STATISTICS);
-
+        context = root.getContext();
+        recyclerView = root.findViewById(R.id.rv_traffic);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.addItemDecoration(new DividerItemDecoration(context, LinearLayoutManager.VERTICAL));
+        try {
+            ArrayList<TrafficModel> trafficModels = new BackgroudAsyncTask().execute(StaticData.URL_TRAFFIC_STATISTICS).get();
+            TrafficAdapter trafficAdapter = new TrafficAdapter(context,trafficModels);
+            recyclerView.setAdapter(trafficAdapter);
+            trafficAdapter.notifyDataSetChanged();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return root;
     }
 
 
-    private class BackgroudAsyncTask extends AsyncTask<String, String, HashMap<String, String>> {
+    private class BackgroudAsyncTask extends AsyncTask<String, String, ArrayList<TrafficModel>> {
         private OkHttpClient client = new OkHttpClient();
         private MediaType JSON =MediaType.get("application/json; charset=utf-8");
 
         @Override
-        protected HashMap<String, String> doInBackground(String... strings) {
+        protected ArrayList<TrafficModel> doInBackground(String... strings) {
             SharedPreferences sharedPreferences = getActivity().getSharedPreferences(StaticData.SHRD_PREF_COOKIE, Context.MODE_PRIVATE);
             String cookie = StaticData.COOKIE_NAME+"="+sharedPreferences.getString(StaticData.COOKIE_NAME, null);
             String result="";
@@ -57,8 +75,13 @@ public class TrafficFragment extends Fragment {
             }
             Log.v("shanu: result",result);
 
+            ArrayList<TrafficModel> trafficModels = new ArrayList<>();
+            for(String trafficStr: result.split("\n")){
+                TrafficModel traffic = new TrafficModel(trafficStr.split(";"));
+                trafficModels.add(traffic);
+            }
 
-            return null;
+            return trafficModels;
         }
         private String makeRequest(String url,String cookie, String json) throws IOException {
             RequestBody body = RequestBody.create(json,JSON);
