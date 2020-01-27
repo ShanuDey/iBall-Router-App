f01e900f629c6ae7e73c513f1ae113be30ffc5c1
ShanuDey <sd4shanudey@gmail.com>
2020-01-27 14:32:18 +0530

    Login: Remove unused FAB
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- app/src/main/java/com/example/iballbaton/MainActivity.java
+++ app/src/main/java/com/example/iballbaton/MainActivity.java
@@ -46,16 +46,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
         SharedPreferences sharedPreferences = getSharedPreferences(StaticData.SHRD_PREF_COOKIE,MODE_PRIVATE);
         String cookie = sharedPreferences.getString(StaticData.COOKIE_NAME,null);
         if(cookie!=null){
