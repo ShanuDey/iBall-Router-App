a4f73b0452b3bb67e7f3462fd2be63241a4f3a7d
ShanuDey <sd4shanudey@gmail.com>
2020-02-03 20:25:25 +0530

    API: extract enable bandwidth control api by reverse engineering
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- app/src/main/java/com/example/iballbaton/StaticData.java
+++ app/src/main/java/com/example/iballbaton/StaticData.java
@@ -5,6 +5,7 @@ public class StaticData {
     public static final String URL_LOGIN = URL_HOST + "login.asp";
     public static final String URL_SYSTEM_STATUS = URL_HOST+ "system_status.asp";
     public static final String URL_TRAFFIC_STATISTICS  = URL_HOST+ "goform/updateIptAccount";
+    public static final String URL_ENABLE_BANDWIDTH_CONTROL = URL_HOST+ "goform/trafficForm?GO=net_tc.asp&up_Band=12800&down_Band=12800&cur_number=2&tc_list_1=80,103,103,0,1,100,1,1&tc_list_2=80,103,103,1,1,100,1,1&tc_enable=";
 
 
     public static final String COOKIE_NAME = "ecos_pw";
