112f2332406a5fa968283b705af81d1d4232991f
ShanuDey <sd4shanudey@gmail.com>
2020-02-07 10:59:36 +0530

    Extract Bandwidth Controller current status fetching api by reverse engineering
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- app/src/main/java/com/example/iballbaton/StaticData.java
+++ app/src/main/java/com/example/iballbaton/StaticData.java
@@ -6,7 +6,7 @@ public class StaticData {
     public static final String URL_SYSTEM_STATUS = URL_HOST+ "system_status.asp";
     public static final String URL_TRAFFIC_STATISTICS  = URL_HOST+ "goform/updateIptAccount";
     public static final String URL_ENABLE_BANDWIDTH_CONTROL = URL_HOST+ "goform/trafficForm?GO=net_tc.asp&up_Band=12800&down_Band=12800&cur_number=2&tc_list_1=80,103,103,0,1,100,1,1&tc_list_2=80,103,103,1,1,100,1,1&tc_enable=";
-
+    public static final String URL_STATUS_BANDWIDTH_CONTROL_ENABLER = URL_HOST+"net_tc.asp";
 
     public static final String COOKIE_NAME = "ecos_pw";
     public static final String SHRD_PREF_COOKIE = "LoginAuth";
